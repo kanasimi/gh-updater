3dc77b3172edbcc607961080e3d348d75808a07c
kanasimi <kanasimi@gmail.com>
2019-08-13 19:53:00 +0800

    add --no-clean

--- _test suite/test.js
+++ _test suite/test.js
@@ -10,13 +10,17 @@ var updater = require('../GitHub.updater.node.js');
 updater.update('kanasimi/CeJS', null, function(version_data) {
 	// /home/travis/build/kanasimi/gh-updater
 	// console.log(process.cwd());
+
 	// has 'CeJS-master'
-	console.log(require('fs').readdirSync('.'));
+	// console.log(require('fs').readdirSync('.'));
 
 	require('../CeJS-master/_for include/node.loader.js');
 
 	CeL.run([ 'data.math', 'application.debug.log' ], function() {
-		CeL.log('test: ' + CeL.GCD(4, 6));
+		CeL.assert(require('fs').readdirSync('.').includes('wikibot-master'),
+				'node gh-updater-cli.js kanasimi/wikibot');
+
+		// CeL.log('test: ' + CeL.GCD(4, 6));
 		CeL.assert([ CeL.GCD(4, 6), 2 ]);
 	});
 });
