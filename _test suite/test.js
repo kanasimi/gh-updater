0941cf82342c8a0b3fa2360c28a08eb7db073177
kanasimi <kanasimi@gmail.com>
2019-08-13 18:38:26 +0800

    fix #2

--- _test suite/test.js
+++ _test suite/test.js
@@ -8,7 +8,7 @@ var updater = require('../GitHub.updater.node.js');
 // @see work_crawler/work_crawler.updater.js
 
 updater.update('kanasimi/CeJS', null, function(version_data) {
-	require('./CeJS-master/_for include/_CeL.loader.nodejs.js');
+	require('./CeJS-master/_for include/node.loader.js');
 
 	CeL.run([ 'data.math', 'application.debug.log' ], function() {
 		CeL.assert([ CeL.GCD(4, 6), 2 ]);
