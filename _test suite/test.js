7e6767bcf63fc240e4637616d1d97e7530325283
kanasimi <kanasimi@gmail.com>
2019-08-13 18:42:23 +0800

    add/fix comments / debug logs

--- _test suite/test.js
+++ _test suite/test.js
@@ -8,6 +8,8 @@ var updater = require('../GitHub.updater.node.js');
 // @see work_crawler/work_crawler.updater.js
 
 updater.update('kanasimi/CeJS', null, function(version_data) {
+	console.log(process.cwd());
+	console.log(require('fs').readdirSync('.'));
 	require('./CeJS-master/_for include/node.loader.js');
 
 	CeL.run([ 'data.math', 'application.debug.log' ], function() {
