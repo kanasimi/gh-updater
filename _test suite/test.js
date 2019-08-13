5ded681ac4a310c2d5aac6b7f40cda6217366e11
kanasimi <kanasimi@gmail.com>
2019-08-13 18:48:44 +0800

    add Build Status

--- _test suite/test.js
+++ _test suite/test.js
@@ -8,9 +8,11 @@ var updater = require('../GitHub.updater.node.js');
 // @see work_crawler/work_crawler.updater.js
 
 updater.update('kanasimi/CeJS', null, function(version_data) {
-	console.log(process.cwd());
-	console.log(require('fs').readdirSync('.'));
-	require('./CeJS-master/_for include/node.loader.js');
+	// /home/travis/build/kanasimi/gh-updater
+	// console.log(process.cwd());
+	// has 'CeJS-master'
+	// console.log(require('fs').readdirSync('.'));
+	require('./kanasimi/gh-updater/CeJS-master/_for include/node.loader.js');
 
 	CeL.run([ 'data.math', 'application.debug.log' ], function() {
 		CeL.assert([ CeL.GCD(4, 6), 2 ]);
