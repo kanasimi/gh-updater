ca3c88e50166d68c8909ae5cfba9602c6a56c619
kanasimi <kanasimi@gmail.com>
2019-08-13 18:30:40 +0800

    add test.js

---
+++ _test suite/test.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+
+// load module
+var updater = require('../GitHub.updater.node.js');
+
+// ============================================================================
+
+// @see work_crawler/work_crawler.updater.js
+
+updater.update('kanasimi/CeJS', null, function(version_data) {
+	var CeL = require('cejs');
+
+	console.log(CeL);
+});
